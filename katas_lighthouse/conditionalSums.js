d98863c9ebeda403ee76dba49bd6284e31ca41c4
David <david.he07@yahoo.ca>
2020-07-22 16:26:38 -0400

    completed conditionalSums.js

---
+++ katas_lighthouse/conditionalSums.js
@@ -0,0 +1,30 @@
+const conditionalSum = function (values, condition) {
+  // Your code here
+
+  let sum = 0;
+
+  if (condition === "even") {
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] % 2 === 0) {
+        sum += values[i];
+      }
+    }
+  }
+
+  if (condition === "odd") {
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] % 2 !== 0) {
+        sum += values[i];
+      }
+    }
+  }
+
+  // console.log(sum);
+
+  return sum;
+};
+
+// console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+// console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+// console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
