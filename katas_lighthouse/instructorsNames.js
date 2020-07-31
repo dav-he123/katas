1691ac5a6662e49f1afbb43953dc328882634bcb
David <david.he07@yahoo.ca>
2020-07-31 17:25:07 -0400

    working on instructors names

--- katas_lighthouse/instructorsNames.js
+++ katas_lighthouse/instructorsNames.js
@@ -1,19 +1,25 @@
 const instructorWithLongestName = function (instructors) {
   // Put your solution here
+
+  for (let key of Object.values(instructors)) {
+    console.log(key.name.);
+
+
+  }
 };
 
-console.log(
-  instructorWithLongestName([
-    { name: "Samuel", course: "iOS" },
-    { name: "Jeremiah", course: "Web" },
-    { name: "Ophilia", course: "Web" },
-    { name: "Donald", course: "Web" },
-  ])
-);
-console.log(
-  instructorWithLongestName([
-    { name: "Matthew", course: "Web" },
-    { name: "David", course: "iOS" },
-    { name: "Domascus", course: "Web" },
-  ])
-);
+// console.log(
+instructorWithLongestName([
+  { name: "Samuel", course: "iOS" },
+  { name: "Jeremiah", course: "Web" },
+  { name: "Ophilia", course: "Web" },
+  { name: "Donald", course: "Web" },
+]);
+// );
+// console.log(
+//   instructorWithLongestName([
+//     { name: "Matthew", course: "Web" },
+//     { name: "David", course: "iOS" },
+//     { name: "Domascus", course: "Web" },
+//   ])
+// );
