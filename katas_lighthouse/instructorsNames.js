83def8eddfa9d21418eda279eec17f36e67ce6ff
David <david.he07@yahoo.ca>
2020-08-04 11:28:45 -0400

    finished instructors names

--- katas_lighthouse/instructorsNames.js
+++ katas_lighthouse/instructorsNames.js
@@ -1,25 +1,30 @@
 const instructorWithLongestName = function (instructors) {
   // Put your solution here
 
-  for (let key of Object.values(instructors)) {
-    console.log(key.name.);
+  // for (let key of Object.values(instructors)) {
+  //   console.log(key.name.split("").length);
+  // }
 
+  let longestName = instructors.sort(function (a, b) {
+    return b.name.length - a.name.length;
+  });
 
-  }
+  // console.log(longestName[0]);
+  return longestName[0];
 };
 
-// console.log(
-instructorWithLongestName([
-  { name: "Samuel", course: "iOS" },
-  { name: "Jeremiah", course: "Web" },
-  { name: "Ophilia", course: "Web" },
-  { name: "Donald", course: "Web" },
-]);
-// );
-// console.log(
-//   instructorWithLongestName([
-//     { name: "Matthew", course: "Web" },
-//     { name: "David", course: "iOS" },
-//     { name: "Domascus", course: "Web" },
-//   ])
-// );
+console.log(
+  instructorWithLongestName([
+    { name: "Samuel", course: "iOS" },
+    { name: "Jeremiah", course: "Web" },
+    { name: "Ophilia", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+console.log(
+  instructorWithLongestName([
+    { name: "Matthew", course: "Web" },
+    { name: "David", course: "iOS" },
+    { name: "Domascus", course: "Web" },
+  ])
+);
