c8877bb5ac2e1b2b65e48cdaf5f34b6326f67f3c
David <david.he07@yahoo.ca>
2020-08-31 17:52:49 -0400

    updated code line for repeating numbers

--- katas_lighthouse/repeatNumbers.js
+++ katas_lighthouse/repeatNumbers.js
@@ -1,18 +1,35 @@
 const repeatNumbers = function (data) {
   // Put your solution here
 
-  let finalArray = [];
+  // let finalArray = [];
+  let output = " ";
+
+  // for (let y = 0; y < data.length; y++) {
+  //   // console.log(data[y]);
+  //   for (let x = 0; x < data[y].length; x++) {
+  //     // console.log(data[y][1]);
+  //     for (let i = data[y][1]; i > 0; i--) {
+  //       finalArray.push(data[y][0]);
+  //       if (i === 1 && y > data.length - 1) {
+  //         finalArray.push(",");
+  //       }
+  //       // return finalArray.join("").toString("");
+  //     }
+  //     return finalArray.join("").toString("");
+  //   }
+  // }
 
   for (let y = 0; y < data.length; y++) {
-    // console.log(data[y]);
-    for (let x = 0; x < data[y].length; x++) {
-      // console.log(data[y][1]);
-      for (let i = 0; i < data[y][1]; i++) {
-        finalArray.push(data[y][0]);
+    let a = data[y][0];
+    let b = data[y][1];
+    for (let x = b; x > 0; x--) {
+      output += a;
+      if (x === 1 && y < data.length - 1) {
+        output += ", ";
       }
-      return finalArray.join("").toString("");
     }
   }
+  return output;
 };
 
 console.log(repeatNumbers([[1, 10]]));
