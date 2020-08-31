fa5e9109f4137fddefd02dffbe1d7b5d5c9cb856
David <david.he07@yahoo.ca>
2020-08-31 12:29:52 -0400

    updated for loop for pushing into array'

--- katas_lighthouse/repeatNumbers.js
+++ katas_lighthouse/repeatNumbers.js
@@ -1,28 +1,31 @@
 const repeatNumbers = function (data) {
   // Put your solution here
 
-  // console.log(data);
+  let finalArray = [];
 
   for (let y = 0; y < data.length; y++) {
     // console.log(data[y]);
-
     for (let x = 0; x < data[y].length; x++) {
-      console.log(data[y][x]);
+      // console.log(data[y][1]);
+      for (let i = 0; i < data[y][1]; i++) {
+        finalArray.push(data[0][0]);
+      }
+      return finalArray;
     }
   }
 };
 
 console.log(repeatNumbers([[1, 10]]));
-console.log(
-  repeatNumbers([
-    [1, 2],
-    [2, 3],
-  ])
-);
-console.log(
-  repeatNumbers([
-    [10, 4],
-    [34, 6],
-    [92, 2],
-  ])
-);
+// console.log(
+//   repeatNumbers([
+//     [1, 2],
+//     [2, 3],
+//   ])
+// );
+// console.log(
+//   repeatNumbers([
+//     [10, 4],
+//     [34, 6],
+//     [92, 2],
+//   ])
+// );
