540492362a4567f7feb92825a9ea70290c432094
David <david.he07@yahoo.ca>
2020-09-01 12:59:56 -0400

    implemented lines for first console log' ' '

--- katas_lighthouse/talkingCalculator.js
+++ katas_lighthouse/talkingCalculator.js
@@ -1,7 +1,33 @@
 const talkingCalendar = function (date) {
   // Your code here
+
+  let array = date.split("/");
+
+  let answer = "";
+
+  for (let i = 0; i < date.length; i++) {
+    if (i === 1) {
+      if (array[i] === "12") {
+        answer += "December" + " ";
+      }
+    } else if (i === 2) {
+      if (array[i] === "02") {
+        answer += "2" + ", ";
+      }
+    }
+  }
+
+  for (let j = 0; j < date.length; j++) {
+    if (j === 0) {
+      if ((array[j] = "2017")) {
+        answer += "2017";
+      }
+    }
+  }
+
+  return answer;
 };
 
 console.log(talkingCalendar("2017/12/02"));
-console.log(talkingCalendar("2007/11/11"));
-console.log(talkingCalendar("1987/08/24"));
+// console.log(talkingCalendar("2007/11/11"));
+// console.log(talkingCalendar("1987/08/24"));
