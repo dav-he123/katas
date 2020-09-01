6810f69c22cde03ebba41ceb4136506fdeec8530
David <david.he07@yahoo.ca>
2020-09-01 10:28:27 -0400

    inputted for loop for maxValue

--- katas_lighthouse/multiplicationTable.js
+++ katas_lighthouse/multiplicationTable.js
@@ -1,5 +1,15 @@
 const multiplicationTable = function (maxValue) {
   // Your code here
+
+  let answer = "";
+
+  for (let i = 0; i < maxValue; i++) {
+    for (let count = 1; count < maxValue; count++) {
+      answer += maxValue * count + " ";
+    }
+  }
+
+  return answer;
 };
 
 console.log(multiplicationTable(1));
