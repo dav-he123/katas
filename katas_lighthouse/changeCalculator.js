a3c98a47be15ac8579bd4673a79fb8fe70a41751
David <david.he07@yahoo.ca>
2020-09-01 16:37:44 -0400

    updated lines for changeCalculator

--- katas_lighthouse/changeCalculator.js
+++ katas_lighthouse/changeCalculator.js
@@ -1,5 +1,37 @@
 const calculateChange = function (total, cash) {
   // Your code here
+
+  let changeInCents = cash - total;
+  let finalAnswer = {};
+
+  let numberOfTenDollars = changeInCents / 1000;
+  let A = Math.trunc(numberOfTenDollars);
+
+  finalAnswer["tenDollar"] = A;
+
+  if (A === 0) {
+    let numberOfFiveDollars = changeInCents / 500;
+    let B = Math.trunc(numberOfFiveDollars);
+
+    finalAnswer["fiveDollar"] = B;
+  } else if (B === 0) {
+    let numberOfTwoDollars = changeInCents / 200;
+    let C = Math.trunc(numberOfTwoDollars);
+
+    finalAnswer["twoDollar"] = C;
+  }
+
+  let numberOfTwoDollars = changeInCents / 200;
+  let C = Math.trunc(numberOfTwoDollars);
+
+  finalAnswer["twoDollar"] = C;
+
+  let numberOfOneDollars = changeInCents / 100;
+  let D = Math.trunc(numberOfOneDollars);
+
+  finalAnswer["oneDollar"] = D;
+
+  return finalAnswer;
 };
 
 console.log(calculateChange(1787, 2000));
