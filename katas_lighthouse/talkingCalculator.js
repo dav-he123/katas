c1ffbcc2958c1c7cf9264e5ba2d10f828e1e7702
David <david.he07@yahoo.ca>
2020-09-01 15:45:49 -0400

    implemented lines for else if

--- katas_lighthouse/talkingCalculator.js
+++ katas_lighthouse/talkingCalculator.js
@@ -7,12 +7,52 @@ const talkingCalendar = function (date) {
 
   for (let i = 0; i < date.length; i++) {
     if (i === 1) {
-      if (array[i] === "12") {
+      if (array[i] === "01") {
+        answer += "January" + " ";
+      } else if (array[i] === "02") {
+        answer += "February" + " ";
+      } else if (array[i] === "03") {
+        answer += "March" + " ";
+      } else if (array[i] === "04") {
+        answer += "April" + " ";
+      } else if (array[i] === "05") {
+        answer += "May" + " ";
+      } else if (array[i] === "06") {
+        answer += "June" + " ";
+      } else if (array[i] === "07") {
+        answer += "July" + " ";
+      } else if (array[i] === "08") {
+        answer += "August" + " ";
+      } else if (array[i] === "09") {
+        answer += "September" + " ";
+      } else if (array[i] === "10") {
+        answer += "October" + " ";
+      } else if (array[i] === "11") {
+        answer += "November" + " ";
+      } else if (array[i] === "12") {
         answer += "December" + " ";
       }
     } else if (i === 2) {
-      if (array[i] === "02") {
+      if (array[i] === "01") {
+        answer += "1" + ", ";
+      } else if (array[i] === "02") {
         answer += "2" + ", ";
+      } else if (array[i] === "03") {
+        answer += "3" + ", ";
+      } else if (array[i] === "04") {
+        answer += "4" + ", ";
+      } else if (array[i] === "05") {
+        answer += "5" + ", ";
+      } else if (array[i] === "06") {
+        answer += "6" + ", ";
+      } else if (array[i] === "07") {
+        answer += "7" + ", ";
+      } else if (array[i] === "08") {
+        answer += "8" + ", ";
+      } else if (array[i] === "09") {
+        answer += "9" + ", ";
+      } else if (array[i] > 9) {
+        answer += array[i] + ", ";
       }
     }
   }
@@ -27,5 +67,5 @@ const talkingCalendar = function (date) {
 };
 
 console.log(talkingCalendar("2017/12/02"));
-// console.log(talkingCalendar("2007/11/11"));
-// console.log(talkingCalendar("1987/08/24"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
