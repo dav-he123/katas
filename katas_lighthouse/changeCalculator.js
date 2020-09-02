8e9cfb6f97aadab513deb86b5a89d49f413723c0
David <david.he07@yahoo.ca>
2020-09-02 14:09:55 -0400

    included lines amount of change

--- katas_lighthouse/changeCalculator.js
+++ katas_lighthouse/changeCalculator.js
@@ -4,24 +4,32 @@ const calculateChange = function (total, cash) {
   let changeInCents = cash - total;
   let finalAnswer = {};
 
-  let numberOfTwentyDollars = changeInCents / 2000;
   let remainder1 = 0;
 
   if (changeInCents >= 2000) {
     remainder1 += changeInCents % 2000;
-  } else if (remainder1 >= 1000) {
-    remainder1 += changeInCents % 1000;
-  } else if (remainder1 >= 500) {
+  }
+  if (changeInCents >= 1000 && changeInCents <= 2000) {
     remainder1 += changeInCents % 1000;
   }
+  if (changeInCents >= 500 && changeInCents <= 1000) {
+    remainder1 += changeInCents % 500;
+  }
+  if (changeInCents >= 200 && changeInCents <= 500) {
+    // remainder1 += changeInCents % 200;
+    // console.log(changeInCents);
+    let numberOfTwoDollars = changeInCents / 200;
+    let twoDollars = Math.trunc(numberOfTwoDollars);
+
+    finalAnswer["twoDollar"] = twoDollars;
+  }
+
+  let numberOfTwentyDollars = changeInCents / 2000;
 
   if (Math.trunc(numberOfTwentyDollars) !== 0) {
     let twentyDollars = Math.trunc(numberOfTwentyDollars);
     finalAnswer["twentyDollar"] = twentyDollars;
   }
-
-  let remainder2 = 0;
-  let remainder3 = 0;
 
   if (Math.trunc(numberOfTwentyDollars) === 0) {
     let numberOfTenDollars = changeInCents / 1000;
@@ -37,10 +45,46 @@ const calculateChange = function (total, cash) {
     }
   }
 
-  let numberOfTwoDollars = remainder1 / 200;
-  let twoDollars = Math.trunc(numberOfTwoDollars);
+  if (changeInCents >= 200 && changeInCents <= 500) {
+    // remainder1 += changeInCents % 200;
+    // console.log(changeInCents);
+    let numberOfTwoDollars = changeInCents / 200;
+    let twoDollars = Math.trunc(numberOfTwoDollars);
 
-  finalAnswer["twoDollar"] = twoDollars;
+    finalAnswer["twoDollar"] = twoDollars;
+  }
+
+  if (changeInCents >= 100 && changeInCents <= 200) {
+    // remainder1 += changeInCents % 200;
+    // console.log(changeInCents);
+    let numberOfOneDollars = changeInCents / 100;
+    let oneDollars = Math.trunc(numberOfOneDollars);
+
+    finalAnswer["oneDollar"] = oneDollars;
+  }
+
+  if (changeInCents >= 25 && changeInCents <= 100) {
+    // remainder1 += changeInCents % 200;
+    // console.log(changeInCents);
+    let numberOfQuarterDollars = changeInCents / 200;
+    let quarterDollars = Math.trunc(numberOfQuarterDollars);
+
+    finalAnswer["quarterDollar"] = quarterDollars;
+  }
+
+  if (changeInCents >= 10 && changeInCents <= 25) {
+    // remainder1 += changeInCents % 200;
+    // console.log(changeInCents);
+    let numberOfQuarterDollars = changeInCents / 200;
+    let quarterDollars = Math.trunc(numberOfQuarterDollars);
+
+    finalAnswer["quarterDollar"] = quarterDollars;
+  }
+
+  // let numberOfTwoDollars = remainder1 / 200;
+  // let twoDollars = Math.trunc(numberOfTwoDollars);
+
+  // finalAnswer["twoDollar"] = twoDollars;
 
   // let numberOfQuarterDollars = changeInCents / 25;
 
@@ -57,6 +101,6 @@ const calculateChange = function (total, cash) {
   return finalAnswer;
 };
 
-console.log(calculateChange(1787, 2000));
+// console.log(calculateChange(1787, 2000));
 console.log(calculateChange(2623, 4000));
-console.log(calculateChange(501, 1000));
+// console.log(calculateChange(501, 1000));
