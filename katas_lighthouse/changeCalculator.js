65ca7f08d9a1ecc45a7b83c21196df13d2d772b8
David <david.he07@yahoo.ca>
2020-09-02 13:21:36 -0400

    implemented lines for remainders

--- katas_lighthouse/changeCalculator.js
+++ katas_lighthouse/changeCalculator.js
@@ -3,11 +3,20 @@ const calculateChange = function (total, cash) {
 
   let changeInCents = cash - total;
   let finalAnswer = {};
-
+  let remainder1 = 0;
   let numberOfTwentyDollars = changeInCents / 2000;
-  let twentyDollars = Math.trunc(numberOfTwentyDollars);
+  if (changeInCents >= 2000) {
+    remainder1 += changeInCents % 2000;
+  } else {
+    remainder1 === 0;
+  }
+  if (Math.trunc(numberOfTwentyDollars) !== 0) {
+    let twentyDollars = Math.trunc(numberOfTwentyDollars);
+    finalAnswer["twentyDollar"] = twentyDollars;
+  }
 
-  finalAnswer["twentyDollar"] = twentyDollars;
+  let remainder2 = 0;
+  let remainder3 = 0;
 
   if (Math.trunc(numberOfTwentyDollars) === 0) {
     let numberOfTenDollars = changeInCents / 1000;
@@ -15,34 +24,46 @@ const calculateChange = function (total, cash) {
 
     finalAnswer["tenDollar"] = tenDollars;
 
+    if (remainder1 >= 1000) {
+      remainder2 += remainder1 % 1000;
+    } else {
+      remainder2 += 0;
+    }
+
     if (tenDollars === 0) {
       let numberOfFiveDollars = changeInCents / 500;
       let fiveDollars = Math.trunc(numberOfFiveDollars);
 
       finalAnswer["fiveDollar"] = fiveDollars;
+
+      if (remainder2 >= 500) {
+        remainder3 += remainder2 % 500;
+      } else {
+        remainder3 += 0;
+      }
     }
   }
 
-  let numberOfTwoDollars = changeInCents / 200;
+  let numberOfTwoDollars = remainder3 / 200;
   let twoDollars = Math.trunc(numberOfTwoDollars);
 
   finalAnswer["twoDollar"] = twoDollars;
 
-  let numberOfQuarterDollars = changeInCents / 25;
+  // let numberOfQuarterDollars = changeInCents / 25;
 
-  if (Math.trunc(numberOfQuarterDollars) % 4 === 0) {
-    let numberOfOneDollars = changeInCents / 100;
+  // if (Math.trunc(numberOfQuarterDollars) % 4 === 0) {
+  //   let numberOfOneDollars = changeInCents / 100;
 
-    if (Math.trunc(numberOfOneDollars) % 2 !== 0) {
-      let oneDollars =
-        Math.trunc(numberOfOneDollars) - (Math.trunc(numberOfOneDollars) - 1);
-      finalAnswer["oneDollar"] = oneDollars;
-    }
-  }
+  //   if (Math.trunc(numberOfOneDollars) % 2 !== 0) {
+  //     let oneDollars =
+  //       Math.trunc(numberOfOneDollars) - (Math.trunc(numberOfOneDollars) - 1);
+  //     finalAnswer["oneDollar"] = oneDollars;
+  //   }
+  // }
 
   return finalAnswer;
 };
 
-// console.log(calculateChange(1787, 2000));
+console.log(calculateChange(1787, 2000));
 console.log(calculateChange(2623, 4000));
-// console.log(calculateChange(501, 1000));
+console.log(calculateChange(501, 1000));
