dac409e6602a495c36e01d7246a9724f8c371988
David <david.he07@yahoo.ca>
2020-09-03 11:07:24 -0400

    put in code for file

---
+++ katas_lighthouse/caseMaker3.js
@@ -0,0 +1,13 @@
+const makeCase = function (input, makeCase) {
+  // Put your solution here
+  console.log(input);
+};
+
+console.log(makeCase("this is a string", "camel"));
+console.log(makeCase("this is a string", "pascal"));
+console.log(makeCase("this is a string", "snake"));
+console.log(makeCase("this is a string", "kebab"));
+console.log(makeCase("this is a string", "title"));
+console.log(makeCase("this is a string", "vowel"));
+console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", ["upper", "snake"]));
