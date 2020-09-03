93b2880075895b0e0cf671ff1420389316273e00
David <david.he07@yahoo.ca>
2020-09-02 22:23:46 -0400

    implemented for loops'

--- katas_lighthouse/organizeInstructors.js
+++ katas_lighthouse/organizeInstructors.js
@@ -1,7 +1,22 @@
 const organizeInstructors = function (instructors) {
   // Put your solution here
 
-  console.log(instructors);
+  let finalObj = {};
+  let finalArray = [];
+
+  for (let elem of instructors) {
+    for (let obj in elem) {
+      finalObj[elem.couse] = finalArray;
+    }
+    finalObj[elem.couse] = finalArray;
+  }
+
+  // for (let elem of instructors) {
+  //   finalArray.push(elem.name);
+  //   finalObj[elem.course] = finalArray;
+  // }
+
+  return finalObj;
 };
 
 console.log(
@@ -13,11 +28,11 @@ console.log(
   ])
 );
 
-console.log(
-  organizeInstructors([
-    { name: "Brendan", course: "Blockchain" },
-    { name: "David", course: "Web" },
-    { name: "Martha", course: "iOS" },
-    { name: "Carlos", course: "Web" },
-  ])
-);
+// console.log(
+//   organizeInstructors([
+//     { name: "Brendan", course: "Blockchain" },
+//     { name: "David", course: "Web" },
+//     { name: "Martha", course: "iOS" },
+//     { name: "Carlos", course: "Web" },
+//   ])
+// );
